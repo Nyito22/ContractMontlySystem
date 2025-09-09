87b9260c7630f166f65713fb77732f93200f4d8d
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-09-09 14:16:14 +0000

    Add project files.

---
+++ ContractMontlySystem/Controllers/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMontlySystem.Controllers
+{
+    public class Employee : Controller
+    {
+        public IActionResult Lecture()
+        {
+            return View();
+        }
+
+        public IActionResult pc()
+        {
+            return View();
+        }
+        public IActionResult am()
+        {
+            return View();
+        }
+        public IActionResult ViewClaim()
+        {
+            return View();
+        }
+    }
+}
---
+++ ContractMontlySystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContractMontlySystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
